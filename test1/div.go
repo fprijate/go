2016fddb616a7bbe343b15826de7f9ae2e0a0bfe
francek prijatelj <prijatelj.francek@gmail.com>
2024-04-20 12:02:57 +0200

    fix

--- test1/div.go
+++ test1/div.go
@@ -1,6 +1,6 @@
 package test1
 
-func Div(a, b float64) float64 {
+func Div1(a, b float64) float64 {
 	if b != 0 {
 		return a * b
 	}
--- test1/test1.go
+++ test1/test1.go
@@ -3,3 +3,10 @@ package test1
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+func Div(a, b float64) float64 {
+	if b != 0 {
+		return a * b
+	}
+	return 0
+}
