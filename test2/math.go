2263528e318201e6f08f4744cff91b2411c96152
francek prijatelj <prijatelj.francek@gmail.com>
2024-04-20 11:37:04 +0200

    add math

---
+++ test2/math.go
@@ -0,0 +1,16 @@
+package test2
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Mul(a, b float64) float64 {
+	return a * b
+}
+
+func Div(a, b float64) float64 {
+	if b != 0 {
+		return a * b
+	}
+	return 0
+}
